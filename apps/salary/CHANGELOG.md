167921dc0f9c726798e897b9b359a3572159e078
weizhan <weizhan@edutao.com>
2016-02-25 18:37:46 +0800

    add salary

---
+++ apps/salary/CHANGELOG.md
@@ -0,0 +1,2 @@
+owncloud-salary (0.0.1)
+* First release
---
+++ apps/salary/tests/integration/AppTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * ownCloud - salary
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author sa <shenyunsheng@edutao.com>
+ * @copyright sa 2016
+ */
+
+use OCP\AppFramework\App;
+use Test\TestCase;
+
+
+/**
+ * This test shows how to make a small Integration Test. Query your class
+ * directly from the container, only pass in mocks if needed and run your tests
+ * against the database
+ */
+class AppTest extends TestCase {
+
+    private $container;
+
+    public function setUp() {
+        parent::setUp();
+        $app = new App('salary');
+        $this->container = $app->getContainer();
+    }
+
+    public function testAppInstalled() {
+        $appManager = $this->container->query('OCP\App\IAppManager');
+        $this->assertTrue($appManager->isInstalled('salary'));
+    }
+
+}
